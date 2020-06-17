715a61ee420fb211b079180fe8d9eee2590237cd
bogdan-todorovic <bogdantodorovic93@gmail.com>
2020-06-17 14:55:42 +0200

    part3 exercises: phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
@@ -16,9 +15,9 @@ const App = () => {
   const [notification, setNotification] = useState(null);
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then(response => setPersons(response.data))
+    personsService
+      .getAll()
+      .then(persons => setPersons(persons));
   }, []);
 
   const personsToShow = search 
@@ -53,7 +52,7 @@ const App = () => {
             .catch(error => {
               setNotification({
                 type: "error",
-                message: `Information of ${changedPerson.name} has already been removed.`
+                message: error.response.data.error
               });
               setTimeout(() => setNotification(null), 5000);
             });
@@ -73,6 +72,13 @@ const App = () => {
           setNotification({
             type: "success",
             message: `Person ${addedPerson.name} successfully added.`
+          });
+          setTimeout(() => setNotification(null), 5000);
+        })
+        .catch(error => {
+          setNotification({
+            type: "error",
+            message: error.response.data.error
           });
           setTimeout(() => setNotification(null), 5000);
         });
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
 const save = person => {
   const request = axios.post(baseUrl, person);
@@ -15,6 +20,7 @@ const update = (id, person) => {
 const remove = id => axios.delete(`${baseUrl}/${id}`);
 
 export default {
+  getAll,
   save,
   update,
   remove
